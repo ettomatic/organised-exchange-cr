fbe42a2b894244b404ec363505bf86e533f895d9
Ettore Berardi <ettore.berardi@bbc.co.uk>
2020-10-10 14:27:00 +0100

    Adds a Config class to handle a json configuration file.

---
+++ src/organised_exchange/config.cr
@@ -0,0 +1,36 @@
+require "json"
+
+module OrganisedExchange
+  class Config
+    CONFIG_FILE = Path["~/.organised_exchange.config"].expand(home: true)
+
+    def self.generate
+      default_config = {source: "file", location: "~/Downloads/calendar.ics"}
+      File.write(CONFIG_FILE, default_config.to_json)
+    end
+
+    def initialize
+      unless File.exists?(CONFIG_FILE)
+        STDERR.puts "#{CONFIG_FILE} not found, run 'organised_exchange init'"
+        exit(1)
+      end
+
+      json = File.read(CONFIG_FILE)
+      @config = Hash(String, String).from_json(json)
+    end
+
+    def source
+      @config["source"]
+    rescue KeyError
+      STDERR.puts "ERROR: source not set in #{CONFIG_FILE}"
+      exit(1)
+    end
+
+    def location
+      @config["location"]
+    rescue KeyError
+      STDERR.puts "ERROR: location not set in #{CONFIG_FILE}"
+      exit(1)
+    end
+  end
+end
